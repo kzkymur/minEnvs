68596604f034d079d08afbb9cb6ff332bfa508d4
山浦一輝 <yamaurakazuki@yamaurakazukinoMacBook-Pro.local>
2020-08-27 11:00:28 +0900

    update React_router(put in withRouter)

--- React_router/src/component/Enter.jsx
+++ React_router/src/component/Enter.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
-export default class Home extends React.Component {
-	render () {
-		return (
-			<h1>Enterだよ！！</h1>
-		)
+const Home = props => {
+	const { history } = props;
+	const toHomePage = () => {
+		history.push('/');
 	}
+	return (
+		<>
+			<Link to="/">Home</Link> {/* これでページ遷移 */}
+			<h1>Enterだよ！！</h1>
+			<button onClick={toHomePage}>toHomePage!</button> {/* 間に処理噛ませたい時はこっち */}
+		</>
+	)
 }
 
